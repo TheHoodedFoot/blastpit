372df31cb8aa17f99fe5d6b1b951c8d43e1075de
Andrew Black <andrew@holly.47or.com>
2014-09-28 15:52:35 +0100

    Initial commit

---
+++ ccLaser/ccLaser.h
@@ -0,0 +1,32 @@
+
+// ccLaser.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CccLaserApp:
+// See ccLaser.cpp for the implementation of this class
+//
+
+class CccLaserApp : public CWinAppEx
+{
+public:
+	CccLaserApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CccLaserApp theApp;
